90dc78e1aa3ab1809cfa4340a284fa910970561d
Tyler Cox <sirtytyz@gmail.com>
2017-02-07 16:41:34 -0600

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name travelAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the travelAppApp
+ */
+angular.module('travelAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name travelAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the travelAppApp
+ */
+angular.module('travelAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
